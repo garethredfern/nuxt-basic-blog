cdf83a7de20d79627cef5392919e8efef199cf70
Gareth Redfern <gareth@garethredfern.com>
2020-09-18 12:54:42 +0100

    :art: update text colour

--- pages/articles/index.vue
+++ pages/articles/index.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="flex justify-center">
       <h2
-        class="text-center text-3xl mb-4 uppercase bg-black text-yellow-200 inline-block mx-auto px-2"
+        class="text-center text-3xl mb-4 uppercase bg-black text-white inline-block mx-auto px-2"
       >
         All Articles
       </h2>
